faad40076773c92588c3ede86849233b5fb6a275
VikasDatir <vikasd27@outlook.com>
2025-09-18 15:47:12 +0530

    updated scripts

--- pro_3/models.py
+++ pro_3/models.py
@@ -1,11 +1,22 @@
 from db import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+class Users(Base):
+    __tablename__ = "Users"
+    uid = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    hash_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+
 
 class Books(Base):
     __tablename__ = "Books"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     rating = Column(Integer)
+    added_by =Column(Integer, ForeignKey("Users.uid"))
+
 
 
 
